bfa1d8a487110b0e9cdf4985dcd6c9980c4c4a37
Deny Prasetyo <jasoet87@gmail.com>
2015-01-05 18:59:15 +0700

    Rename WebApplicationContextProvider

--- src/main/scala/org/altairtoolkit/ApplicationContextProvider.scala
+++ src/main/scala/org/altairtoolkit/ApplicationContextProvider.scala
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
  * Deny Prasetyo,S.T
  * Java(Scala) Developer and Trainer
- * Software Engineer | Java Virtual Machine Junkies!
+ * Software Engineer | Java Virtual Machine Junkie!
  * jasoet87@gmail.com
  * <p/>
  * http://github.com/jasoet
@@ -14,15 +14,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  *
  */
 
-class ApplicationContextProvider(classes: Array[Class[_]]) extends AnnotationConfigApplicationContext {
-  classes.foreach { c =>
-    register(c)
-  }
-  refresh()
-}
-
 object ApplicationContextProvider {
   def apply(classes: Array[Class[_]]): ApplicationContext = {
-    new ApplicationContextProvider(classes)
+    val applicationContext = new AnnotationConfigApplicationContext()
+    applicationContext.register(classes: _*)
+    applicationContext.refresh()
+    applicationContext
   }
 }
