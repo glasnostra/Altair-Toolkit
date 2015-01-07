9edee43089ee674b851219a1629a91f413133579
Deny Prasetyo <jasoet87@gmail.com>
2015-01-07 20:13:27 +0700

    Stick to Version 1

--- project/Build.scala
+++ project/Build.scala
@@ -53,7 +53,7 @@ object Build extends Build {
   
   val Organization = "org.altairtoolkit"
   val Name = "Altair-Toolkit"
-  val Version = "1.0-ALPHA"
+  val Version = "1.0"
   val ScalaVersion = "2.11.4"
   val ScalatraVersion = "2.3.0"
   val SpringVersion = "4.1.1.RELEASE"
